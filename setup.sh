c94c282b85dc5be123c092c23680be9cb5eae1e6
Nick Travers <n.e.travers@gmail.com>
2018-07-13 01:49:18 +0000

    Build tmux from source

--- setup.sh
+++ setup.sh
@@ -15,7 +15,6 @@ sudo apt-get install --yes \
   libxtst6 \
   make \
   tar \
-  tmux \
   xserver-xephyr \
   vim
 
@@ -23,9 +22,25 @@ green 'Setting up directories ...'
 DEV_DIR=~/Development
 mkdir -p "$DEV_DIR"
 
+green 'Building tmux ...'
+
+sudo apt-get install \
+  autogen \
+  automake \
+  libevent-dev \
+  libncurses5-dev \
+  pkg-config
+
+cd "$DEV_DIR" && git clone git@github.com:tmux/tmux.git
+cd tmux && \
+  sh autogen.sh && \
+  autoreconf -i --force && ./configure && make && \
+  sudo make install
+
 green 'Setting up dotfiles ...'
 DOTFILES_DIR=~/Development/dotfiles
 rm -rf "$DOTFILES_DIR" && mkdir -p "$DOTFILES_DIR" && cd "$DOTFILES_DIR"
+
 git clone git@github.com:nicktrav/dotfiles.git .
 git fetch origin nickt.debian && git checkout nickt.debian
 ./install.sh
